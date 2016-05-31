e7ef1131c909bd1ebb13efcfb1ec4c469fb2e262
Lucas Barbula <lucas.barbula@gmail.com>
2016-05-31 17:09:27 -0600

    working

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
 
-        $('#movieSend').click(function(){
-          var search = $('#filmTitle').val()
-          sessionStorage.setItem('film', search)
+    $('#movieSend').click(function() {
+        var search = $('#filmTitle').val()
+        sessionStorage.setItem('film', search)
+    })
+    var movie = sessionStorage['film'];
+    console.log(movie)
+    $.get('https://galvanize-cors-proxy.herokuapp.com/http://www.omdbapi.com/?t=' + movie, function(data) {
+        var actors = data.Actors;
+        var awards = data.Awards;
+        var poster = data.Poster;
+        $('#actors').append(actors)
+        $('#awards').append(awards)
+        $('#poster').attr('src', poster)
+        console.log(data)
+    })
+    $('form').submit(function(event) {
+        event.preventDefault();
+        var post = {
+            title: $('#title').val(),
+            genre: $('#genre').val(),
+            description: $('#description').val(),
+            coverPicture: $('#image').val(),
+            rating: $('#rating').val()
+        }
+        console.log(post)
+        $.post("http://mighty-eyrie-15280.herokuapp.com/films", post).done(function(message) {
+            $('.save-status').text(message.message);
+            $('.save-status').fadeIn(500).delay(2000).fadeOut(500);
         })
-        var movie = sessionStorage['film'];
-        console.log(movie)
-        $.get('http://www.omdbapi.com/?t=' + movie, function(data) {
-            var actors = data.Actors;
-            var awards = data.Awards;
-            var poster = data.Poster;
-             $('#actors').append(actors)
-             $('#awards').append(awards)
-             $('#poster').attr('src', poster)
-            console.log(data)
-    })
-    $('#comments').submit(function(){
-      event.preventDefault();
-    })
-    $('#movieSend').click(function(){
-      var film = ('#filmTitle').val();
     })
 })
