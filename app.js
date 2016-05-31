6f3cf83551fd3f2e75c97d0f8edc0d4df68418a1
Lucas Barbula <lucas.barbula@gmail.com>
2016-05-31 11:42:20 -0600

    deployed, link added

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $("h1").click(function(){
     alert("working")
   })
+  $.get('https://api.themoviedb.org/3/movie/550/images?api_key=da165806dfc2a36b2b7e5f3599c39b5c', function(data){
+    
+  })
 })
